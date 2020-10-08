e4d6d34e663a7aa2a97196abf8545958eaf012b1
David Fan <fand.kanade@gmail.com>
2020-10-08 00:24:58 -0400

    Renamed project and apps

--- journal/journal/wsgi.py
+++ journal/journal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for trader project.
+WSGI config for journal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'trader.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'journal.settings')
 
 application = get_wsgi_application()
